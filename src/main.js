8fc798289cf0e70c77d7eb20f94c21da0f898153
AlenaGM <alena.guillaume4@gmail.com>
2022-12-02 21:51:42 +0100

    quit with apolllo

--- src/main.js
+++ src/main.js
@@ -1,10 +1,4 @@
 import { createApp } from "vue";
-import { DefaultApolloClient } from "@vue/apollo-composable";
-import {
-  ApolloClient,
-  createHttpLink,
-  InMemoryCache,
-} from "@apollo/client/core";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
@@ -12,19 +6,4 @@ import "./style.css";
 
 const pinia = createPinia();
 
-const httpLink = createHttpLink({
-  uri: "https://rickandmortyapi.com/graphql",
-});
-
-const cache = new InMemoryCache();
-
-const apolloClient = new ApolloClient({
-  link: httpLink,
-  cache,
-});
-
-createApp(App)
-  .provide(DefaultApolloClient, apolloClient)
-  .use(pinia)
-  .use(router)
-  .mount("#app");
+createApp(App).use(pinia).use(router).mount("#app");
