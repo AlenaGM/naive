a627fe8bd9ad6d0cff5c626325d7b9b20ee16080
AlenaGM <alena.guillaume4@gmail.com>
2022-12-08 11:56:33 +0100

    upd

--- src/components/layout/menu.js
+++ src/components/layout/menu.js
@@ -1,27 +1,27 @@
 const menu = [
   {
     name: "магазин",
-    image: "/svg/menu-shop.svg",
+    image: "/naive/svg/menu-shop.svg",
     path: "/naive/",
   },
   {
     name: "новости",
-    image: "/svg/menu-news.svg",
+    image: "/naive/svg/menu-news.svg",
     path: "/naive/news",
   },
   {
     name: "как заказать",
-    image: "/svg/menu-order.svg",
+    image: "/naive/svg/menu-order.svg",
     path: "/naive/how-to-order",
   },
   {
     name: "доставка",
-    image: "/svg/menu-delivery.svg",
+    image: "/naive/svg/menu-delivery.svg",
     path: "/naive/delivery",
   },
   {
     name: "контакты",
-    image: "/svg/menu-contacts.svg",
+    image: "/naive/svg/menu-contacts.svg",
     path: "/naive/contacts",
   },
 ];
