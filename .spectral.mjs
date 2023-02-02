1743286a47785f8073ddf9169378a96753cfd343
alena.guillaume4@gmail.com <alena.guillaume4@gmail.com>
2023-02-02 14:31:43 +0100

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE4MjAwNw/spectral.js?branch_slug=main&token=eb2e1514-a281-40f8-93ca-e59b2d4b7d2b";
+export default { extends: ruleset };
