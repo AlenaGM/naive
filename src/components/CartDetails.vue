ed5cdd4b0c56b74250fb9c7fe56ebe023c85e600
AlenaGM <alena.guillaume4@gmail.com>
2023-02-08 20:31:26 +0100

    upd cart

--- src/components/CartDetails.vue
+++ src/components/CartDetails.vue
@@ -1,21 +1,52 @@
 <template>
   <div class="cart">
     <h2 class="cart__title">корзина</h2>
-    <div class="cart__table">
-      <div class="cart__table_head">head</div>
-      <div class="cart__table_row">
-        <div class="cart__table_column">column</div>
+    <div class="cart__table table">
+      <div class="table__head">
+        <div class="table__head_label">Товар</div>
+        <div class="table__head_label">Наименование</div>
+        <div class="table__head_label">Стоимость</div>
+        <div class="table__head_label">Удалить</div>
       </div>
-      <div class="cart__table_total">total</div>
+      <div class="table__row">
+        <div class="table__column"><img src="img/item-1.jpg" /></div>
+        <div class="table__column">
+          <div>Очень красивая картинка</div>
+          <div>Дарья Воробьева</div>
+          <div>2011</div>
+        </div>
+        <div class="table__column">
+          <div>18000</div>
+        </div>
+        <div class="table__column">delete</div>
+      </div>
+      <div class="table__row">
+        <div class="table__column"><img src="img/item-1.jpg" /></div>
+        <div class="table__column">
+          <div>Очень красивая картинка</div>
+          <div>Дарья Воробьева</div>
+          <div>2011</div>
+        </div>
+        <div class="table__column">
+          <div>18000</div>
+        </div>
+        <div class="table__column">delete</div>
+      </div>
+      <div class="table__row">
+        <div class="table__column"><img src="img/item-1.jpg" /></div>
+        <div class="table__column">
+          <div>Очень красивая картинка</div>
+          <div>Дарья Воробьева</div>
+          <div>2011</div>
+        </div>
+        <div class="table__column">
+          <div>18000</div>
+        </div>
+        <div class="table__column">delete</div>
+      </div>
+      <div class="table__total">total</div>
     </div>
     <div class="cart__order">ОФОРМИТЬ ЗАКАЗ</div>
-    <div v-for="artwork of cartStore.cart">
-      <img :src="artwork.thumbnail" />
-      <div>{{ artwork.title }}</div>
-      <div>{{ artwork.artistName }}</div>
-      <div>{{ artwork.created }}</div>
-      <div>{{ artwork.price }}</div>
-    </div>
   </div>
 </template>
 
@@ -29,11 +60,9 @@ const cartStore = useCartStore();
 <style lang="scss" scoped>
 .cart {
   display: flex;
+  flex-grow: 1;
   flex-direction: column;
   align-items: flex-start;
-  @media screen and (max-width: 1092px) {
-    align-items: center;
-  }
   &__title {
     font-size: 2rem;
     font-weight: 600;
@@ -46,25 +75,83 @@ const cartStore = useCartStore();
     }
   }
   &__table {
-    display: grid;
-    align-items: start;
-    @media screen and (max-width: 1092px) {
-      grid-template-columns: 1fr;
-      max-width: 660px;
-    }
-    @media screen and (max-width: 400px) {
-      margin-bottom: 84px;
-    }
-    &_head {
-    }
-    &_column {
-    }
-    &_row {
-    }
-    &_total {
-    }
+    width: 100%;
   }
   &__order {
+    background-color: aquamarin;
+  }
+}
+.table {
+  display: grid;
+  @media screen and (max-width: 1092px) {
+    grid-template-columns: 1fr;
+    max-width: 660px;
+  }
+  @media screen and (max-width: 400px) {
+    margin-bottom: 84px;
+  }
+  &__head {
+    padding: 9px 0;
+    display: grid;
+    grid-template-columns: 160px 1fr 160px 160px;
+    column-gap: 40px;
+    align-items: center;
+    border-top: 1px solid var(--third-black);
+    border-bottom: 1px solid var(--third-black);
+    &_label {
+      align-items: center;
+      color: var(--semi-black);
+      font-size: 16px;
+      font-weight: 500;
+      &:nth-last-of-type(2),
+      &:last-of-type {
+        text-align: center;
+      }
+    }
+    @media screen and (max-width: 1092px) {
+      display: none;
+    }
+  }
+  &__row {
+    display: grid;
+    grid-template-columns: 160px 1fr 160px 160px;
+    column-gap: 40px;
+    border-bottom: 1px solid var(--third-black);
+    align-items: center;
+    padding: 20px 0;
+    @media screen and (max-width: 1092px) {
+      display: block;
+    }
+  }
+  &__column {
+    position: relative;
+    &:nth-last-of-type(2),
+    &:last-of-type {
+      text-align: center;
+    }
+    img {
+      width: 100%;
+      @media screen and (max-width: 1092px) {
+        max-width: 200px;
+        object-fit: cover;
+      }
+    }
+  }
+  &__total {
+    padding: 5px 16px;
+    display: grid;
+    grid-template-columns: 160px 1fr 1fr 160px;
+    column-gap: 10px;
+    align-items: center;
+    border-bottom: 2px solid #eeeff4;
+    background: #fff;
+    &_label {
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+      color: #999;
+      cursor: pointer;
+    }
   }
 }
 </style>
