397c2db46017d8e5c1a78d556c8bfdc9145c0b75
AlenaGM <alena.guillaume4@gmail.com>
2023-02-09 18:57:55 +0100

    remade cart scss

--- src/components/CartDetails.vue
+++ src/components/CartDetails.vue
@@ -1,57 +1,42 @@
 <template>
   <div class="cart">
     <h2 class="cart__title">корзина</h2>
-    <div class="cart__table table">
-      <div class="table__head">
-        <div class="table__head_label">Товар</div>
-        <div class="table__head_label">Наименование</div>
-        <div class="table__head_label">Стоимость</div>
-        <div class="table__head_label">Удалить</div>
-      </div>
-      <div class="table__row">
-        <div class="table__column"><img src="img/item-1.jpg" /></div>
-        <div class="table__column">
-          <div>Очень красивая картинка</div>
-          <div>Дарья Воробьева</div>
-          <div>2011</div>
-        </div>
-        <div class="table__column">
-          <div>18000</div>
-        </div>
-        <div class="table__column">delete</div>
-      </div>
-      <div class="table__row">
-        <div class="table__column"><img src="img/item-1.jpg" /></div>
-        <div class="table__column">
-          <div>Очень красивая картинка</div>
-          <div>Дарья Воробьева</div>
-          <div>2011</div>
-        </div>
-        <div class="table__column">
-          <div>18000</div>
-        </div>
-        <div class="table__column">delete</div>
-      </div>
-      <div class="table__row">
-        <div class="table__column"><img src="img/item-1.jpg" /></div>
-        <div class="table__column">
-          <div>Очень красивая картинка</div>
-          <div>Дарья Воробьева</div>
-          <div>2011</div>
-        </div>
-        <div class="table__column">
-          <div>18000</div>
-        </div>
-        <div class="table__column">delete</div>
-      </div>
-      <div class="table__total">total</div>
-    </div>
+    <table class="cart__table table">
+      <thead>
+        <tr class="table__row">
+          <th>Товар</th>
+          <th>Наименование</th>
+          <th>Стоимость</th>
+          <th>Удалить</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="table__row">
+          <td><img src="img/item-1.jpg" /></td>
+          <td>
+            <span>Очень красивая картинка</span>
+            <div>Дарья Воробьева</div>
+            <div>2011</div>
+          </td>
+          <td><span>18 000 R</span></td>
+          <td>
+            Удалить
+            <img src="svg/trash.svg" alt="delete icon" />
+          </td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <tr class="table__row">
+          <th class="table__total">Итого:</th>
+          <th class="table__sum"><span>18 000 R</span></th>
+        </tr>
+      </tfoot>
+    </table>
     <div class="cart__order">ОФОРМИТЬ ЗАКАЗ</div>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
 import { useCartStore } from "@/store/cart.js";
 
 const cartStore = useCartStore();
@@ -76,81 +61,97 @@ const cartStore = useCartStore();
   }
   &__table {
     width: 100%;
+    margin-bottom: 53px;
   }
   &__order {
-    background-color: aquamarin;
+    background-color: yellow;
   }
 }
 .table {
-  display: grid;
-  @media screen and (max-width: 1092px) {
-    grid-template-columns: 1fr;
-    max-width: 660px;
-  }
-  @media screen and (max-width: 400px) {
-    margin-bottom: 84px;
-  }
-  &__head {
-    padding: 9px 0;
-    display: grid;
-    grid-template-columns: 160px 1fr 160px 160px;
-    column-gap: 40px;
-    align-items: center;
-    border-top: 1px solid var(--third-black);
-    border-bottom: 1px solid var(--third-black);
-    &_label {
-      align-items: center;
-      color: var(--semi-black);
-      font-size: 16px;
-      font-weight: 500;
-      &:nth-last-of-type(2),
-      &:last-of-type {
-        text-align: center;
-      }
-    }
-    @media screen and (max-width: 1092px) {
-      display: none;
-    }
+  display: flex;
+  flex-direction: column;
+  font-size: 1rem;
+  font-weight: 500;
+  color: var(--black);
+  span {
+    color: inherit;
+    font-size: 18px;
+    font-weight: 600;
   }
   &__row {
-    display: grid;
-    grid-template-columns: 160px 1fr 160px 160px;
+    display: flex;
     column-gap: 40px;
+    padding: 20px 0;
     border-bottom: 1px solid var(--third-black);
     align-items: center;
-    padding: 20px 0;
-    @media screen and (max-width: 1092px) {
-      display: block;
+  }
+  thead,
+  tfoot {
+    color: var(--semi-black);
+  }
+  thead {
+    border-top: 1px solid var(--third-black);
+    tr {
+      padding: 8px 0;
     }
   }
-  &__column {
-    position: relative;
-    &:nth-last-of-type(2),
-    &:last-of-type {
-      text-align: center;
+  td,
+  th {
+    font-weight: 500;
+    &:first-of-type {
+      display: flex;
+      align-items: flex-start;
+      justify-content: flex-start;
+      width: 160px;
+      img {
+        width: 100%;
+      }
     }
-    img {
-      width: 100%;
-      @media screen and (max-width: 1092px) {
-        max-width: 200px;
-        object-fit: cover;
+    &:nth-of-type(2) {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      min-width: 250px;
+      flex-grow: 1;
+      span {
+        flex-wrap: wrap;
+      }
+      div:nth-of-type(2) {
+        font-size: 14px;
+      }
+    }
+    &:nth-of-type(3) {
+      display: flex;
+      justify-content: flex-start;
+      width: 120px;
+    }
+    &:last-of-type {
+      width: 140px;
+      display: flex;
+      justify-content: flex-start;
+      flex-direction: row;
+      img {
+        width: 20px;
+        margin-left: 8px;
       }
     }
   }
-  &__total {
-    padding: 5px 16px;
-    display: grid;
-    grid-template-columns: 160px 1fr 1fr 160px;
-    column-gap: 10px;
-    align-items: center;
-    border-bottom: 2px solid #eeeff4;
-    background: #fff;
-    &_label {
+  tfoot {
+    span {
+      color: var(--black);
+      font-size: 18px;
+      font-weight: 600;
+    }
+    .table__total {
       display: flex;
+      min-width: 410px;
+      flex-grow: 1;
+      justify-content: flex-end;
+    }
+    .table__sum {
+      display: flex;
+      max-width: 300px;
       justify-content: flex-start;
-      align-items: center;
-      color: #999;
-      cursor: pointer;
     }
   }
 }
