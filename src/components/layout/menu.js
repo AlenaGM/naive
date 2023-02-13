c4dee19b175bae80c955c1f08be086c26217a32e
AlenaGM <alena.guillaume4@gmail.com>
2023-02-13 21:07:54 +0100

    upd paths

--- src/components/layout/menu.js
+++ src/components/layout/menu.js
@@ -1,27 +1,27 @@
 const menu = [
   {
     name: "магазин",
-    image: "/svg/menu-shop.svg",
+    image: "/naive/svg/menu-shop.svg",
     path: "/",
   },
   {
     name: "художники",
-    image: "/svg/menu-artist.svg",
+    image: "/naive/svg/menu-artist.svg",
     path: "/artists",
   },
   {
     name: "новости",
-    image: "/svg/menu-news.svg",
+    image: "/naive/svg/menu-news.svg",
     path: "/news",
   },
   {
     name: "заказ и доставка",
-    image: "/svg/menu-delivery.svg",
+    image: "/naive/svg/menu-delivery.svg",
     path: "/delivery",
   },
   {
     name: "контакты",
-    image: "/svg/menu-contacts.svg",
+    image: "/naive/svg/menu-contacts.svg",
     path: "/contacts",
   },
 ];
