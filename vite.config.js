463fdbfbb2cc76d7c59e9fb60a54cb91e119dfa1
AlenaGM <alena.guillaume4@gmail.com>
2023-02-13 20:54:40 +0100

    upd

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/naiveart.netlify.app/",
+  base: "/naive/",
   plugins: [vue()],
   resolve: {
     alias: {
